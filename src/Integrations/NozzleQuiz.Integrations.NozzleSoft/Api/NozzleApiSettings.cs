fc423520b8741278f287feac729c82c2bec4054e
Gökhan Doğru <info@gokhandogru.net>
2022-06-13 00:08:08 +0300

    added images

--- src/Integrations/NozzleQuiz.Integrations.NozzleSoft/Api/NozzleApiSettings.cs
+++ src/Integrations/NozzleQuiz.Integrations.NozzleSoft/Api/NozzleApiSettings.cs
@@ -5,7 +5,7 @@
         public const string ApiUrl = "https://apidemo.nozzlesoft.com/api/"; // TODO: encryption
         public const string Username = "candidate@nozzlesoft.com"; // TODO: encryption
         public const string Password = "123456"; // TODO: encryption
-        public const string AuthorizationKey = "e286f8b3-1cb3-4ae9-b702-0ed3e365cf31"; // TODO: encryption
+        public const string AuthorizationKey = "35f2c1af-2364-4331-a10c-3ca6fb279131"; // TODO: encryption
         public const string GuestAuthorizationKey = "NozzleGuest"; // TODO: encryption
     }
 }
